777487a3494a1f058de2565264ee7b2069a70c86
M. Firhan Azmi Nor <muhammadfirhan@student.ub.ac.id>
2020-02-10 14:46:59 +0700

    halaman display

---
+++ resources/views/display.blade.php
@@ -0,0 +1,187 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700" rel="stylesheet">
+
+    <title>Hello, world!</title>
+    <style type="text/css">
+      body {
+        background-color: #cfd8dc;
+      }
+
+.fixed-top {
+  height: 120px;
+  background-color: #2196f3;
+  color: white;
+}
+
+.card-panggilan {
+  font-family: 'Open Sans', sans serif;
+  font-size: 80px;
+  font-weight: bold;
+  width: 600px;
+  margin: 80px auto;
+  border-radius: 10px;
+  perspective: 1400px;
+  color: #fff;
+  background: #2196f3;
+  display: flex;
+  border-radius: 10px;
+  justify-content: center;
+  align-items: center;
+  backface-visibility: hidden;
+  text-align: center;
+}
+.container-fluid {
+  margin-top: 100px;
+}
+.header-card-loket {
+  font-family: 'Open Sans', sans serif;
+  background: #2196f3;
+  color: #fff;
+  font-weight: bold;
+  text-align: center; 
+}
+    </style>
+  </head>
+  <body>
+    <div class="fixed-top">
+    <h3>KPP PRATAMA BIAK</h3>
+    <p>Jl. Adibai No. 1 Sumberkar</p>
+    <p style="float: right;">08:21:27 - Senin, 10 Februari 2020</p>
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-6">
+          <div class="card card-panggilan">
+  <div class="card-body">ANTRIAN<br> B305<br> KE LOKET 1</div>
+</div>
+        </div>
+        <div class="col-6 mt-5 pt-4">
+            <div class="row">
+                <div class="col-4 mb-2">
+                    <div class="card">
+                        <div class="card-header header-card-loket">
+                            LOKET 1
+                        </div>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0 text-center">
+                                <p>B301</p>
+                            </blockquote>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4 mb-2">
+                    <div class="card">
+                        <div class="card-header header-card-loket">
+                            LOKET 1
+                        </div>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0 text-center">
+                                <p>B301</p>
+                            </blockquote>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4 mb-2">
+                    <div class="card">
+                        <div class="card-header header-card-loket">
+                            LOKET 1
+                        </div>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0 text-center">
+                                <p>B301</p>
+                            </blockquote>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4 mb-2">
+                    <div class="card">
+                        <div class="card-header header-card-loket">
+                            LOKET 1
+                        </div>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0 text-center">
+                                <p>B301</p>
+                            </blockquote>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4 mb-2">
+                    <div class="card">
+                        <div class="card-header header-card-loket">
+                            LOKET 1
+                        </div>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0 text-center">
+                                <p>B301</p>
+                            </blockquote>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4 mb-2">
+                    <div class="card">
+                        <div class="card-header header-card-loket">
+                            LOKET 1
+                        </div>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0 text-center">
+                                <p>B301</p>
+                            </blockquote>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4 mb-2">
+                    <div class="card">
+                        <div class="card-header header-card-loket">
+                            LOKET 1
+                        </div>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0 text-center">
+                                <p>B301</p>
+                            </blockquote>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4 mb-2">
+                    <div class="card">
+                        <div class="card-header header-card-loket">
+                            LOKET 1
+                        </div>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0 text-center">
+                                <p>B301</p>
+                            </blockquote>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4 mb-2">
+                    <div class="card">
+                        <div class="card-header header-card-loket">
+                            LOKET 1
+                        </div>
+                        <div class="card-body">
+                            <blockquote class="blockquote mb-0 text-center">
+                                <p>B301</p>
+                            </blockquote>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
